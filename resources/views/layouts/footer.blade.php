149595a903281fa8174289389c8bea77df368c7e
gusgusteam <109000871+gusgusteam@users.noreply.github.com>
2023-05-29 11:56:46 -0800

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
-    <script src="{{asset('/lib_basico/jquery.slim.min.js')}}" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="{{asset('/lib_basico/bootstrap.bundle.min.js')}}" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <script src="{{asset('/lib_basico/jquery.slim.min.js')}}"  crossorigin="anonymous"></script>
+    <script src="{{asset('/lib_basico/bootstrap.bundle.min.js')}}" crossorigin="anonymous"></script>
     
     
     <script src="{{asset('/lib_basico/jquery-3.5.1.js')}}"></script>
