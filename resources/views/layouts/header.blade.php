ae13646a50034be797d6c2c0671e30e61326fcad
gusgusteam <109000871+gusgusteam@users.noreply.github.com>
2023-05-29 11:57:01 -0800

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PELICULA DIRECTOR</title>
-    <link rel="stylesheet" href="{{asset('/lib_basico/bootstrap.min.css')}}" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('/lib_basico/bootstrap.min.css')}}" crossorigin="anonymous">
 
 </head>
 <body>
